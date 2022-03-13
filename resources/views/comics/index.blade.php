3001acbc8c83e0a99b584e8417642d53bbaef6c1
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-13 22:06:27 +0100

    Stampati prodotti in tabella index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,5 +4,29 @@
 
 @section('content')
     <h1>Comics</h1>
-    @dump($comics)
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Comic</th>
+                <th scope="col">Description</th>
+                <th scope="col">Sale Date</th>
+                <th scope="col">Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic)
+                <tr>
+                    <td><img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                        <div>
+                            <strong>{{ $comic->title }}</strong>
+                            ({{ $comic->type }})
+                            <br>
+                            {{ $comic->series }} series
+                    <td>{!! $comic->description !!}</td>
+                    <td>{{ $comic->sale_date }}</td>
+                    <td>{{ $comic->price }}€</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
