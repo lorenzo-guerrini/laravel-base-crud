e1dbc517410da10fafcfcdbaa63d75ef16d644e2
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-15 23:27:27 +0100

    Aggiunti errori per ciascun campo in edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -5,49 +5,82 @@
 @endsection
 
 @section('content')
+    {{-- @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif --}}
+
     <form action="{{ route('comics.update', $comic->id) }}" method="POST">
         @csrf
         @method('PUT')
 
         <div class="form-group">
             <label for="title">Title</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}"
-                placeholder="Comic's title">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+                value="{{ old('title') ?? $comic->title }}" placeholder="Comic's title">
+            @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="description">Description</label>
-            <textarea class="form-control" style="height: 150px" id="description" name="description"
-                placeholder="Comic's description">{{ $comic->description }}</textarea>
+            <textarea class="form-control @error('description') is-invalid @enderror" style="height: 150px" id="description"
+                name="description"
+                placeholder="Comic's description">{{ old('description') ?? $comic->description }}</textarea>
+            @error('description')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="thumb">Thumb URL</label>
-            <input type="text" class="form-control" id="thumb" name="thumb" value="{{ $comic->thumb }}"
-                placeholder="Comic's thumb URL">
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb"
+                value="{{ old('thumb') ?? $comic->thumb }}" placeholder="Comic's thumb URL">
+            @error('thumb')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="price">Price</label>
-            <input type="number" class="form-control" id="price" name="price" value="{{ $comic->price }}"
-                placeholder="Comic's price">
+            <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" name="price"
+                value="{{ old('price') ?? $comic->price }}" min="0" placeholder="Comic's price">
+            @error('price')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="series">Series</label>
-            <input type="text" class="form-control" id="series" name="series" value="{{ $comic->series }}"
-                placeholder="Comic's series">
+            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series"
+                value="{{ old('series') ?? $comic->series }}" placeholder="Comic's series">
+            @error('series')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="sale_date">Sale Date</label>
-            <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}">
+            <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date"
+                value="{{ old('sale_date') ?? $comic->sale_date }}">
+            @error('sale_date')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="type">Type</label>
-            <input type="text" class="form-control" id="type" name="type" value="{{ $comic->type }}"
-                placeholder="Comic's type">
+            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type"
+                value="{{ old('type') ?? $comic->type }}" placeholder="Comic's type">
+            @error('type')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="text-center pt-2">
